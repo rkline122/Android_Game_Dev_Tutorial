97f51a439301dea7b8897a4d146760fe23907c19
Fletcher <fletchjosh246@gmail.com>
2022-11-01 21:24:11 -0400

    Finished jumping animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,36 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float jump;
+
+    public LayerMask ground;
+
     private Rigidbody2D rigidBody;
+    private Collider2D playerCollider;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+
+        bool grounded = Physics2D.IsTouchingLayers(playerCollider, ground);
+
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)){
+            if(grounded) {
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, jump);
+            } 
+        }
+
+        animator.SetBool("Grounded", grounded);
     }
 }
 
